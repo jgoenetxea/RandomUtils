817dd8ccea59fe38bcda984d65d19efbef812e47
goe <jgoenetxea@gmail.com>
2017-09-22 17:39:55 +0200

    Included sample to read torch '.t7' files.

---
+++ python/TorchManagement/extract_t7_file_data.py
@@ -0,0 +1,9 @@
+'''
+The content of this file is based on the work published by Brendan Shillingford on github:
+    https://github.com/bshillingford/python-torchfile
+Please, refer to the github site for more information.
+'''
+import torchfile
+stuff = torchfile.load('test.t7')
+
+print("File loaded")
